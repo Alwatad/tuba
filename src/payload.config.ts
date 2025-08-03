cf7a91d904749beba1a0ddc47086650b07c52813
Abdullah <121164316+3km-0@users.noreply.github.com>
2025-08-03 16:11:01 +0300

    feat: customize admin title

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -70,7 +70,7 @@ export default buildConfig({
         },
       ],
       title: "Admin Panel",
-      titleSuffix: "| Mandala SH",
+      titleSuffix: "| Tuba",
     },
     importMap: {
       baseDir: path.resolve(dirname),
